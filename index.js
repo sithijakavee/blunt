ac66e9aa01e9110734a18b02c8e22a8f8e596512
sithijakavee <sithijakavee120@gmail.com>
2023-04-17 15:54:57 +0530

    port config

--- index.js
+++ index.js
@@ -88,6 +88,6 @@ app.post("/webhooks", async (req, res) => {
 
 })
 
-app.listen(5000, () => {
-    console.log("Server listening on port 5000")
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server listening")
 })
