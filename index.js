ae6921f0f69e3ede50f363764854f270be443e10
sithijakavee <sithijakavee120@gmail.com>
2023-04-21 10:26:23 +0530

    adhsud

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ app.post("/checkout", async (req, res) => {
         })
     }
     catch (error) {
+        console.log(error)
         res.status(500).json({
             error: error
         })
