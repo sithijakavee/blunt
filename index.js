c5b2855d594561b35002eb8f615acf8798a6669b
sithijakavee <sithijakavee120@gmail.com>
2023-04-21 10:28:07 +0530

    swdsd

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post("/checkout", async (req, res) => {
         })
     }
     catch (error) {
-        console.log(error)
+        console.log("My Error: ", error)
         res.status(500).json({
             error: error
         })
